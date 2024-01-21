85af4dc2b5035343b89c5950a12712c63fef8a0b
Livio <livio5261@gmail.com>
2024-01-22 00:24:45 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    messages = []
+    
+    if request.method == "GET":
+        for message in Message.query.all():
+            messages.append(message.to_dict())
+        
+        response = make_response(jsonify(messages), 200)
+        return(response)
+    
+    elif request.method == "POST": 
+        data = request.get_json()               
+        new_message = Message(body=data.get('body'), username=data.get('username'))
+        # new_message = Message(body = request.form.get('body'), username = request.form.get('username'))
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response = make_response(jsonify(new_message.to_dict()), 201)
+        return(response)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods = ["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+    if request.method == "GET":
+        response = make_response(jsonify(message.to_dict()), 200)
+        return(response)
+    
+    elif request.method == "PATCH":
+        message = Message.query.filter_by(id = id).first()
+        data = request.get_json()
+        
+        for attr in data:
+            setattr(message, attr, data.get(attr))
+            
+        db.session.add(message) 
+        db.session.commit() 
+            
+        response = make_response(jsonify(message.to_dict()))
+        
+        return(response)
+    
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        
+        response = make_response(
+            jsonify({'deleted': True}),
+            200,
+        )
+        return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
